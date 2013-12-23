83b2bb02ea0a98b26372b02434dcce517e9d0237
姚丽梅 <yaolimei@xiaomi.com>
2013-12-23 19:03:15 +0800

    ceshi

--- feed.php
+++ feed.php
@@ -12,7 +12,7 @@
  * $Author: liubo $
  * $Id: feed.php 17217 2011-01-19 06:29:08Z liubo $
 */
-
+/* ceshi */
 define('IN_ECS', true);
 define('INIT_NO_USERS', true);
 define('INIT_NO_SMARTY', true);
