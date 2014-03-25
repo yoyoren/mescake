0472547f1267c8130c3ebc22cea652e37037473f
姚丽梅 <yaolimei@xiaomi.com>
2014-03-25 14:13:56 +0800

    add cat huodong

--- sql/cat_activity.sql
+++ sql/cat_activity.sql
@@ -3,5 +3,8 @@
  weibo_name varchar(32) not null,
  img varchar(256) not null,
  status int(4) not null,
- times int(32) not null
+ times int(32) not null,
+ add_time varchar(32) not null,
 );
+
+ALTER TABLE cat_activity ADD add_time varchar( 32 ) NOT NULL DEFAULT '0';
