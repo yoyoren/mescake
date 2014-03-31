cb4765fa141ec1a3cee3650591d25b20d768d4e4
姚丽梅 <yaolimei@xiaomi.com>
2014-03-31 10:33:09 +0800

    revert huodong

--- weibo/config.php
+++ weibo/config.php
@@ -3,4 +3,4 @@ header('Content-Type: text/html; charset=UTF-8');
 define( "WB_AKEY" , '375795715' );
 define( "WB_SKEY" , '0f76e6eb7599987f1deb74183dccf6c4' );
 //define( "WB_CALLBACK_URL" , 'www.mescake.com/weibo/callback.php' );
-define( "WB_CALLBACK_URL" , 'test.mescake.com/weibo/callback.php' );
+define( "WB_CALLBACK_URL" , 'huodong.mescake.com/weibo/callback.php' );
