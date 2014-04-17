72d682ecb358375ec284a3d0dbedfa5c2429decd
姚丽梅 <yaolimei@xiaomi.com>
2014-04-17 21:26:42 +0800

    bugfix

--- lib/catogary.php
+++ lib/catogary.php
@@ -1,62 +1,9 @@
 <?php
-$CAKE_CATO = array(
- 
-  'specialday'=>array(
-    'name'=>'纪念日',
-	'cato'=>array(
-		array('name'=>'taster','title'=>'For Taster / 献给品赏家','data'=>array('1','5','6','7','8','9','11')),
-		array('name'=>'lover','title'=>'For Lover / 献给爱人','data'=>array('27','26','22','25')),
-		array('name'=>'explorer','title'=>'For Explorer / 献给旅行家','data'=>array('15','16','17','18','19','20','21')),
-		array('name'=>'fun','title'=>'For Fun / 来点气氛','data'=>array('32')),
-	),
-	'img'=>'mg/test-img.png',
-	'desc'=>'在Ta的生日或者特殊纪念日，送上特殊的祝福',
-	'detail_desc_h1'=>'在生日或特殊的日子里，送上你最甜美的祝福',
-	'detail_desc_h2'=>'<a href="#taster">献给品赏家</a> | <a href="#lover">献给爱人</a> | <a href="#explorer">献给旅行家</a>',
-	'cls' => 'for-day',
-  ),
-  'forlove'=>array(
-    'name'=>'为了爱',
-	'cato'=>array(
-		array('name'=>'promise','title'=>'Promise / 许诺','data'=>array('22','25','26','27')),
-		array('name'=>'travle','title'=>'Travle / 一起旅行','data'=>array('15','16','18','19')),
-	),
-	'img'=>'mg/test-img.png',
-	'desc'=>'懵懂或热烈，都在这刻永恒',
-	'detail_desc_h1'=>'懵懂或永恒，都在这刻永恒',
-	'detail_desc_h2'=>'<a href="#promise">爱的许诺</a> | <a href="#travle">一起旅行</a>',
-	'cls' => 'for-love',
-  ),
-   'party'=>array(
-    'name'=>'欢聚一下',
-	'cato'=>array(
-		array('name'=>'classic','title'=>'Classic / 环球经典','data'=>array('33','35','37','38')),
-		array('name'=>'world','title'=>'Around World / 异国风味','data'=>array('15','16','17','18','19','20','21')),
-		array('name'=>'share','title'=>'分享时刻','data'=>array('9','1','5','6','7','11','8','70')),
-		array('name'=>'fun','title'=>'For Fun / 来点气氛','data'=>array('32'))
-	),
-	'img'=>'mg/test-img.png',
-	'desc'=>'无论Party还是私聚，一同享用精挑细选之作吧!',
-	'detail_desc_h1'=>'无论Party还是私聚，都可以一同享用精挑细选之作',
-	'detail_desc_h2'=>'<a href="#classic">环球经典</a> | <a href="#world">异国风味</a> | <a href="#share">分享时刻</a> | <a href="#fun">来点气氛</a>',
-	'cls' => 'for-happy',
-  ),
-   'dream'=>array(
-    'name'=>'梦想家',
-	'cato'=>array(),
-	'img'=>'mg/test-img.png',
-	'desc'=>'不同人有不同的梦想以及对心目中理想美味的定义',
-	'detail_desc_h1'=>'不同的人有不同的梦想，以及对心目中理想美味的定义',
-	'detail_desc_h2'=>'',
-	'detail_desc_h3'=>'Dream / 田园',
-	'cls' => 'for-dream',
-  ),
-);
 $CAKE_CATO = file_get_contents("goodsconfig.json");
 $CAKE_CATO = json_decode($CAKE_CATO,true);
 
 $CAKE_SLIDER = array(
-  array('img'=>'img/luckman.png',title=>'','desc'=>'','link'=>'http://huodong.mescake.com/cake/32'),
+  array('img'=>'img/luckman.png',title=>'','desc'=>'','link'=>'http://www.mescake.com/cake/32'),
   array('img'=>'img/banner-huodong.jpg',title=>'','desc'=>'','link'=>'http://huodong.mescake.com/cat'),
   array('img'=>'img/banner-cat.jpg',title=>'<h2 style="color:#f59e21;">像猫一样享受生活</h2>','desc'=>'<p style="width:340px;">晒太阳是特长，看风景是爱好，<br>快乐随性是我的天分；<br>得意洋洋的享受时间，<br>让整个世界做我的游乐场。</p>','link'=>'/catcake'),
   
