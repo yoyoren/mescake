bb89950a4c188db767d628f92f0d9792c3a1e4c9
姚丽梅 <yaolimei@xiaomi.com>
2014-05-22 19:54:54 +0800

    add proxy

--- proxy.php
+++ proxy.php
@@ -5,4 +5,4 @@ document.domain = 'mescake.com';
 define('IN_ECS', true);
 require(dirname(__FILE__) . '/includes/init.php');
 ?>
-<script src="http://static.n.mescake/touch/zepto.min.js"></script>
+<script src="http://static.n.mescake.com/touch/zepto.min.js"></script>
