7ef93f34df42d44261d00b8ea06b878dc3d5443f
姚丽梅 <yaolimei@xiaomi.com>
2014-05-22 19:06:12 +0800

    add ajax proxy

---
+++ proxy.php
@@ -0,0 +1,8 @@
+﻿<script>
+document.domain = 'mescake.com';
+</script>
+<?php
+define('IN_ECS', true);
+require(dirname(__FILE__) . '/includes/init.php');
+?>
+<script src="http://10.237.113.51/touch/zepto.min.js"></script>
