26e39f2713ed54891151586792843e12be4c8bb4
RENYUAN <hanlongsen@21cake.com>
2015-10-16 11:22:00 +0800

    self_station ui

---
+++ mes_self_station_op.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+<meta charset="utf-8">
+<style>
+body,table,tr{font-size:12px;}
+</style>
+</head>
+
+<body>
+<div>
+添加自提站点
+<table>
+<tr>
+  <td>站点名称</td>
+  <td><input id="" width="200"/></td>
+</tr>
+<tr>
+  <td>站点地址</td>
+  <td><input id="" width="200"/></td>
+</tr>
+<tr>
+  <td>站点所在省份</td>
+  <td><input id="" width="200"/></td>
+</tr>
+<tr>
+    <td>站点所在城市</td>
+    <td><input id="" width="200"/></td>
+</tr>
+<tr>
+    <td>站点所在区域</td>
+    <td><input id="" width="200"/></td>
+</tr>
+<tr>
+    <td>站点经度</td>
+    <td><input id="" width="200"/></td>
+</tr>
+<tr>
+  <td>站点纬度</td>
+  <td><input id="" width="200"/></td>
+</tr>
+</table>
+<button>确认添加</button>
+</div>
+<div>
+已添加的自提站点列表:
+<table>
+<tr>
+  <td>站点名称</td>
+  <td>站点地址</td>
+  <td>站点所在省份</td>
+  <td>站点所在城市</td>
+  <td>站点所在区域</td>
+  <td>站点经度</td>
+  <td>站点纬度</td>
+</tr>
+</table>
+</div>
+</body>
+</html>
+
